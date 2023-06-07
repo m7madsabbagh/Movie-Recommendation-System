f6652a1049f0b92037234e021e30355fc40c4fd9
m7madsabbagh <125734929+m7madsabbagh@users.noreply.github.com>
2023-06-07 17:24:38 +0300

    Add files via upload

---
+++ LoginForm.cpp
@@ -0,0 +1,15 @@
+﻿#include "LoginForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main()
+//int main (array＜String^＞^args )
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	LoginForm::LoginForm form;
+	Application::Run(% form);
+	return 0;
+}
