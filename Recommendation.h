a0d36151fa8d8d4bec64bd1c13907aa4eff6a036
m7madsabbagh <125734929+m7madsabbagh@users.noreply.github.com>
2023-07-26 18:57:38 +0300

    Recommed.h

--- Recommendation.h
+++ Recommendation.h
@@ -1,17 +1,53 @@
 #pragma once
 
+#include <msclr\marshal.h>
+#include <msclr\marshal_cppstd.h>
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include <msclr\marshal.h>
+#include <msclr\marshal_cppstd.h>
 #include "Movie.h"
+#include "User.h"
 
 using namespace System;
-using namespace System::Collections::Generic;
+using namespace System::IO;
+using namespace System::Net;
 using namespace MySql::Data::MySqlClient;
+using namespace System::Net::Http;
+using namespace System::Threading::Tasks;
+using namespace System::Collections::Generic;
 
-public ref class RecommendationHandler
-{
-public:
-    RecommendationHandler();
-    List<Movie^>^ getRecommendedMovies(String^ Username);
-private:
-    MySqlConnection^ con;
-    List<String^>^ getUserGenres(String^ Username);
-};
+namespace LoginForm {
+
+    public ref class Recommed : public System::Windows::Forms::Form
+    {
+    private:
+    private:
+
+        List<String^>^ userGenrePreferences;
+        List<Movie^>^ allMovies;
+        HttpClient^ client;
+        System::String^ Username;
+    private: System::Windows::Forms::FlowLayoutPanel^ flowLayoutPanel;
+
+
+        System::ComponentModel::Container^ components;
+
+
+
+    public:
+        Recommed(List<String^>^ userGenres, List<Movie^>^ allMovies, String^ LoggedInUsername);
+        void LoadLikedGenres(String^ username);
+        void GetMovieListAsync(String^ genre);
+        String^ GetGenreId(String^ genre);
+        void HandleMovieListResponse(Task<HttpResponseMessage^>^ responseTask);
+        void CreatePictureBoxWithTitle(String^ path, String^ title);
+        void InitializeComponent(void);
+        delegate void PictureBoxWithTitleDelegate(String^ path, String^ title);
+
+    
+   
+    };
+
+}
